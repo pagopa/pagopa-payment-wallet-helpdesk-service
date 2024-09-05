08e4e61c8d93c8fadd43f0ea7ca0117ba85388cd
Giovanni Berti <giovanni.berti@pagopa.it>
2024-09-05 16:41:00 +0200

    feat: use build number from file instead of hardcoded

--- cmd/app/server.go
+++ cmd/app/server.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"context"
+	_ "embed"
 	. "pagopa.it/pagopa-payment-wallet-helpdesk-service/cmd/app/api"
 )
+
+//go:embed version.txt
+var version string
 
 type Server struct{}
 
 var _ StrictServerInterface = (*Server)(nil)
 
 func (*Server) GetServiceInfo(_ context.Context, _ GetServiceInfoRequestObject) (GetServiceInfoResponseObject, error) {
-	return GetServiceInfo200JSONResponse{Version: "1.0.0"}, nil
+	return GetServiceInfo200JSONResponse{Version: version}, nil
 }
